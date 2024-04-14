f3f84fe9366d65d2f552475690fe49a3a4196889
Pawel Lamza <pawel.lamza@swmansion.com>
2024-04-14 13:48:49 +0200

    set up default phoenix server up and running

---
+++ robo_soccer_platform/test/robo_soccer_platform_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule RoboSoccerPlatformWeb.ErrorJSONTest do
+  use RoboSoccerPlatformWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert RoboSoccerPlatformWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert RoboSoccerPlatformWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
