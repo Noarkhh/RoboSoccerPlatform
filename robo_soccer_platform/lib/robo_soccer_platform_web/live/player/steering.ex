ff095f233ec22cd496758ead0e18b939ec61684d
noarkhh <jakub.pryc@gmail.com>
2024-09-29 04:18:06 +0200

    Remove commented and unused code

--- robo_soccer_platform/lib/robo_soccer_platform_web/live/player/steering.ex
+++ robo_soccer_platform/lib/robo_soccer_platform_web/live/player/steering.ex
@@ -1,33 +1,15 @@
 defmodule RoboSoccerPlatformWeb.Player.Steering do
-  require Logger
   use RoboSoccerPlatformWeb, :live_view
 
+  require Logger
   alias RoboSoccerPlatform.Player
   alias RoboSoccerPlatformWeb.Endpoint
-  alias RoboSoccerPlatformWeb.Player.Steering.Utils
-
-  require Logger
 
   @game_state "game_state"
-  @controller "controller"
-  @disconnect "disconnect"
-
-  # @spec unregister(String.t()) :: :ok
-  # def unregister(player_id) do
-  # Logger.debug("Unregistering player #{player_id}")
-
-  # RoboSoccerPlatformWeb.Endpoint.broadcast_from(
-  # self(),
-  # @controller,
-  # "unregister_player",
-  # player_id
-  # )
-  # end
 
   @impl true
   def mount(params, _session, socket) do
     Endpoint.subscribe(@game_state)
-    # Endpoint.subscribe(@disconnect)
 
     player = %Player{id: params["id"], team: params["team"], username: params["username"]}
 
@@ -73,13 +55,6 @@ defmodule RoboSoccerPlatformWeb.Player.Steering do
     {x, ""} = Integer.parse(x_str)
     {y, ""} = Integer.parse(y_str)
 
-    # Endpoint.broadcast_from(self(), @controller, "joystick_position", %{
-    # x: x / 100,
-    # y: y / 100,
-    # id: socket.assigns.player.id,
-    # room_code: socket.assigns.room_code
-    # })
-
     RoboSoccerPlatform.GameController.update_player_input(
       socket.assigns.player.id,
       x / 100,
@@ -89,43 +64,8 @@ defmodule RoboSoccerPlatformWeb.Player.Steering do
     {:noreply, socket}
   end
 
-  def handle_event("restoreState", token_data, socket) when is_binary(token_data) do
-    Endpoint.broadcast_from(self(), @controller, "register_player", socket.assigns.player)
-    RoboSoccerPlatformWeb.Player.PlayersMonitor.monitor(self(), socket.assigns.player.id)
-
-    case Utils.restore_from_token(token_data) do
-      {:ok, nil} ->
-        socket
-
-      {:ok, %{game_state: game_state}} ->
-        case game_state do
-          "started" ->
-            push_event(socket, "game_started", %{})
-
-          "stopped" ->
-            push_event(socket, "game_stopped", %{})
-        end
-
-      {:error, reason} ->
-        Logger.debug("Error while restoring state: #{reason}")
-
-        socket
-        |> put_flash(:error, reason)
-        |> clear_browser_storage()
-    end
-    |> then(&{:noreply, &1})
-  end
-
-  def handle_event("restoreState", _token_data, socket) do
-    Endpoint.broadcast_from(self(), @controller, "register_player", socket.assigns.player)
-    RoboSoccerPlatformWeb.Player.PlayersMonitor.monitor(self(), socket.assigns.player.id)
-
-    {:noreply, socket}
-  end
-
   def handle_info(%{topic: @game_state, event: "start_game"}, socket) do
     socket
-    |> store(%{game_state: "started"})
     |> push_event("game_started", %{})
     |> then(&{:noreply, &1})
   end
@@ -133,35 +73,7 @@ defmodule RoboSoccerPlatformWeb.Player.Steering do
   @impl true
   def handle_info(%{topic: @game_state, event: "stop_game"}, socket) do
     socket
-    |> store(%{game_state: "stopped"})
     |> push_event("game_stopped", %{})
     |> then(&{:noreply, &1})
-  end
-
-  def handle_info(%{topic: @disconnect, event: "disconnect", payload: %{id: id}}, socket)
-      when id != socket.assigns.id do
-    {:noreply, socket}
-  end
-
-  def handle_info(%{topic: @disconnect, event: "disconnect", payload: %{id: id}}, socket) do
-    {:noreply, push_navigate(socket, to: "/player")}
-  end
-
-  defp store(socket, data) do
-    push_event(socket, "store", %{
-      key: socket.assigns.player.id,
-      data: Utils.serialize_to_token(data)
-    })
-  end
-
-  defp restore(socket, event \\ "restoreState") do
-    push_event(socket, "restore", %{
-      key: socket.assigns.player.id,
-      event: event
-    })
-  end
-
-  defp clear_browser_storage(socket) do
-    push_event(socket, "clear", %{key: socket.assigns.player.id})
   end
 end
