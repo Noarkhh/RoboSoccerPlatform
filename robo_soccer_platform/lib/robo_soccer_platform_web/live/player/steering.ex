c5a4211937f78e0332dc07001540b6e43566c86e
Paweł <pawellamza@gmail.com>
2024-12-22 11:46:54 +0100

    when game is stopped hide joystick for player

--- robo_soccer_platform/lib/robo_soccer_platform_web/live/player/steering.ex
+++ robo_soccer_platform/lib/robo_soccer_platform_web/live/player/steering.ex
@@ -2,8 +2,9 @@ defmodule RoboSoccerPlatformWeb.Player.Steering do
   use RoboSoccerPlatformWeb, :live_view
 
   require Logger
-  alias RoboSoccerPlatform.Player
   alias RoboSoccerPlatformWeb.Endpoint
+  alias RoboSoccerPlatform.GameController
+  alias RoboSoccerPlatform.Player
 
   @game_state "game_state"
 
@@ -18,14 +19,17 @@ defmodule RoboSoccerPlatformWeb.Player.Steering do
     player = %Player{id: id, team: team, username: username}
 
     if connected?(socket) do
-      RoboSoccerPlatform.GameController.register_player(player, self())
+      GameController.register_player(player, self())
     end
 
-    if RoboSoccerPlatform.GameController.room_code_correct?(room_code) do
-      case RoboSoccerPlatform.GameController.get_game_state() do
+    if GameController.room_code_correct?(room_code) do
+      game_state = GameController.get_game_state()
+
+      case game_state do
         :started -> push_event(socket, "game_started", %{})
         _not_started -> push_event(socket, "game_stopped", %{})
       end
+      |> assign(game_state: game_state)
       |> assign(player: player)
       |> then(&{:ok, &1})
     else
@@ -52,23 +56,34 @@ defmodule RoboSoccerPlatformWeb.Player.Steering do
       |> assign(team_display: team_display)
 
     ~H"""
-    <div class="text-3xl text-center">
-      <%= @team_display %>
-    </div>
-    <div class="text-3xl text-center">
-      <%= @player.username %>
-    </div>
+    <div class="flex flex-col gap-4">
+      <div class="text-3xl text-center">
+        <%= @team_display %>
+      </div>
+      <div class="text-3xl text-center">
+        <%= @player.username %>
+      </div>
 
-    <div id="container">
-      <div
-        id="joystick"
-        phx-hook="JoyStick"
-        phx-update="ignore"
-        data-inner_joystick_color={@inner_joystick_color}
-        data-outer_joystick_color={@outer_joystick_color}
-        class={"fixed w-[min(70vw,70vh)] h-[min(70vw,70vh)] left-0 top-0 right-0 bottom-0 m-auto"}
-      >
-        <!-- joystick will be rendered here -->
+      <div id="container">
+        <div
+          id="joystick"
+          phx-hook="JoyStick"
+          phx-update="ignore"
+          data-inner_joystick_color={@inner_joystick_color}
+          data-outer_joystick_color={@outer_joystick_color}
+          class={"w-[min(70vw,70vh)] h-[min(70vw,70vh)] m-auto"}
+        >
+          <!-- joystick will be rendered here -->
+        </div>
+      </div>
+
+      <div :if={@game_state == :stopped} class="absolute left-0 top-6 right-0 bottom-0 bg-sky-blue">
+        <div class="text-3xl font-bold text-center mt-64">
+          Rozgrywka wstrzymana przez organizatora.
+        </div>
+        <div class="text-3xl font-bold text-center">
+          Proszę czekać...
+        </div>
       </div>
     </div>
     """
@@ -79,7 +94,7 @@ defmodule RoboSoccerPlatformWeb.Player.Steering do
     {x, ""} = Integer.parse(x_str)
     {y, ""} = Integer.parse(y_str)
 
-    RoboSoccerPlatform.GameController.update_player_input(
+    GameController.update_player_input(
       socket.assigns.player.id,
       x / 100,
       y / 100
@@ -90,6 +105,7 @@ defmodule RoboSoccerPlatformWeb.Player.Steering do
 
   def handle_info(%{topic: @game_state, event: "start_game"}, socket) do
     socket
+    |> assign(game_state: :started)
     |> push_event("game_started", %{})
     |> then(&{:noreply, &1})
   end
@@ -97,6 +113,7 @@ defmodule RoboSoccerPlatformWeb.Player.Steering do
   @impl true
   def handle_info(%{topic: @game_state, event: "stop_game"}, socket) do
     socket
+    |> assign(game_state: :stopped)
     |> push_event("game_stopped", %{})
     |> then(&{:noreply, &1})
   end
