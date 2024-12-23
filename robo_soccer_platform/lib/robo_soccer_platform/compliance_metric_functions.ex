c4947a8d3108e7f5f9a04119946e56754aed6ab2
Paweł <pawellamza@gmail.com>
2024-12-23 15:11:57 +0100

    updated euclidean distance to show cooperation instead of lack of cooperation

--- robo_soccer_platform/lib/robo_soccer_platform/compliance_metric_functions.ex
+++ robo_soccer_platform/lib/robo_soccer_platform/compliance_metric_functions.ex
@@ -5,15 +5,16 @@ defmodule RoboSoccerPlatform.CooperationMetricFunctions do
 
   @doc "returns value from range [0, 1]; 0 - best cooperation, 1 - worst cooperation"
   @spec euclidean_distance([GameController.player_input()], float(), float()) :: float()
-  def euclidean_distance([], _, _), do: 0.0
-  def euclidean_distance([_player_input], _, _), do: 0.0
+  def euclidean_distance([], _, _), do: 1.0
+  def euclidean_distance([_player_input], _, _), do: 1.0
   def euclidean_distance(player_inputs, aggregated_x, aggregated_y) do
-    player_inputs
-    |> Enum.map(fn %{x: x, y: y} ->
-      :math.sqrt(:math.pow(x - aggregated_x, 2) + :math.pow(y - aggregated_y, 2))
-    end)
-    |> Enum.sum()
-    |> Kernel./(length(player_inputs))
-    |> Kernel./(:math.sqrt(2))
+    euclidean_distance =
+      player_inputs
+      |> Enum.map(fn %{x: x, y: y} ->
+        :math.sqrt(:math.pow(x - aggregated_x, 2) + :math.pow(y - aggregated_y, 2))
+      end)
+      |> Enum.sum()
+
+    1 - (euclidean_distance / (length(player_inputs) * :math.sqrt(2)))
   end
 end
